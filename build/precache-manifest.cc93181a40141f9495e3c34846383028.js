660a72efc2e3f881994bf5223a8c7aa96c1cfd62
obulareddyveera <obulareddy.veera@gmail.com>
2020-09-23 18:39:16 +0530

    Deploying EasyFarm

---
+++ build/precache-manifest.cc93181a40141f9495e3c34846383028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9716644d2a0d7722de386c65354c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c51fb6bfea489b2e7133",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "151a799adaa86a8e578e",
+    "url": "/static/js/2.73d6eaca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.73d6eaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51fb6bfea489b2e7133",
+    "url": "/static/js/main.68858ae6.chunk.js"
+  },
+  {
+    "revision": "42fe1421aa65f222ca09",
+    "url": "/static/js/runtime-main.e068c0cc.js"
+  }
+]);
