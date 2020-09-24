d4ea29a32341d8c70f937c4e1cb387e76b98c833
obulareddyveera <obulareddy.veera@gmail.com>
2020-09-24 12:03:36 +0530

    Setting up home.

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,6 +1,18 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import HNavBar from "./../components/navbar/hNavbar";
 
 export default () => {
-  return <Button color="primary">VEERA awesome!!</Button>;
-};
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HNavBar />
+      <Container fixed>
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './containers/home';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+import "./index.css";
+import Home from "./containers/home";
+import * as serviceWorker from "./serviceWorker";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "0px !important",
+    padding: "0px !important",
+  },
+}));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider>
+    <React.StrictMode>
+      <div style={useStyles.root}>
+        <Home />
+      </div>
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
